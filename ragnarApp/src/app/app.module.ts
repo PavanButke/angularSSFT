27b77ba48b11da673f36f56afb3e7d8d7c3691ae
PavanButke <butkepavan55@gmail.com>
2023-07-20 23:56:54 +0530

    created components

--- ragnarApp/src/app/app.module.ts
+++ ragnarApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
