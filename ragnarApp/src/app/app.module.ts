715193997dc26da3396b5d6b8545512351efe5e7
PavanButke <butkepavan55@gmail.com>
2023-07-20 19:02:41 +0530

    login component

--- ragnarApp/src/app/app.module.ts
+++ ragnarApp/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,LoginComponent , SignupComponent]
 })
 export class AppModule { }
